d61522cd9a3603cf44eeafd5b67390e62c47ca9e
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-15 07:27:10 -0700

    New commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-/*Function main*/
-int main(void)
-{
-puts("Programming is like building a multilingual puzzle ");
-return 0;
-}
+     /*Function main*/
+     int main(void)
+                  { 
+                  puts("Programming is like building a multilingual puzzle");
+                  return 0;
+                  }
