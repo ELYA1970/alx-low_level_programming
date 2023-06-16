0f40cc07ec1cbc36678f72171cc8313d7b528f33
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-16 10:40:46 -0700

    New commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	char alphabet[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+	char alphabet[27] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'
-			, 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+			, 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\0'};
 	int i = 0;
 
 	while (i < 26)
