c3af8e06134bae86e610bb4338d62ebd994541fc
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-16 05:05:01 -0700

    New commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	if (n>0)
+	{
+		printf("the number is positive\n");
+	}
+	else if (n<0)
+	{
+		printf("the number is negative\n");
+	}
+	else
+	{
+		printf("the number is zero\n");
+	}
+	return (0);
+}
