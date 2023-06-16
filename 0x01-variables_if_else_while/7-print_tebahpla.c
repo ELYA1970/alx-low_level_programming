735f5413d5941bc3a171e3309fc9d1cec46da1e4
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-16 15:29:07 -0700

    New commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'z' ; r >= 'a' ; r--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
