910d52d9c1cab4422879c33e30f860d303cf3021
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-20 09:06:11 -0700

    correct errors task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,12 @@
+#include "main.h"
+/**
+ *_islower - print if a character is lowercase or not
+ *
+ * Return: Always 0
+ */
 int _islower(int c)
 {
+	/*  c is an integer represents code ASCII of character */
 	if (c >= 97 && c <= 122)
 		return (1);
 	else
