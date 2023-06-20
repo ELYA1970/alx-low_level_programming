a727b983a603e4170fa4ebe9076c9dfe84bfa6b9
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-20 07:33:30 -0700

    task for functions/nested loops projects

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet from 'a' to 'z'
+ *
+ * Return: No return
+ */
 void print_alphabet(void)
 {
 	int i = 97;
+
 	while (i <= 122)
 	{
-		putchar(i);
-		i++
+		_putchar(i);
+		i++;
 	}
-}	
+	_putchar('\n');
+}
