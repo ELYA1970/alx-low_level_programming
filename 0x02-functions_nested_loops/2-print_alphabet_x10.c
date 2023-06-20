0f0875dc12db5443295fd74e7138ae7e13486b6b
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-19 20:43:05 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+void print_alphabet_x10(void)
+{
+	int j = 0;
+	while (j < 10)
+	{
+		int i = 97;
+        while (i <= 122)
+        {
+                putchar(i);
+                i++
+        }
+	j++;
+	}
+}
