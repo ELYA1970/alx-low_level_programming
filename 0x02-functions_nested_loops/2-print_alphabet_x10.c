a0c8b8263974e35d0c6f96cef8197bcda051ac54
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-20 08:44:53 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include  "main.h"
 /**
- * print_aalphabet_x10 - print alphabet from 'a' to 'z' 10 times
+ * print_alphabet_x10 - print alphabet from 'a' to 'z' 10 times
  *
  * Return: No Return
  */
@@ -12,11 +12,12 @@ void print_alphabet_x10(void)
 	{
 		int i = 97;
 
-        while (i <= 122)
-        {
-                _putchar(i);
-                i++;
-        }
+		while (i <= 122)
+		{
+			_putchar(i);
+			i++;
+		}
+	_putchar('\n');
 	j++;
 	}
 }
