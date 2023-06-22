5c55ce83bc80b34ededd0bff7a984448d61a1eca
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-22 05:01:59 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print "01356789"
+ * Return: no return
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48 ; c <= 57 ; c++)
+	{
+	       if (c != 50 && c != 52)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
