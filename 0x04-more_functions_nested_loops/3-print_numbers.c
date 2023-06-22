0457b1d3d8779b7cc9619c1791431a6b4482ee03
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-22 04:26:34 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from '0' to '9'
+ * Return: no return
+ */
+void print_numbers(void)
+{
+int c;
+
+for (c = 48 ; c <= 57 ; c++)
+	_putchar(c);
+_putchar('\n');
+}
