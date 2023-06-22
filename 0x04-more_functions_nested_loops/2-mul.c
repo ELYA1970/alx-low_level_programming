eb15587aac7245f05dff677cb17cc704c60555b5
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-22 03:55:31 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - print the multiplication between two integers
+ * @a: integer entered by user
+ * @b: integer entered by user
+ * Return: no return
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
