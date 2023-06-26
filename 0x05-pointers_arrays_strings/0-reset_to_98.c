4842910fe2f652a7fc57b8dcec3126926781aaa2
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-25 22:14:52 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - take a number and reset to 98
+ * @*n: pointe to a number entered by user
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
