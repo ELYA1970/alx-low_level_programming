74c6b38c3a2e48d4814600e07ed5085e42808663
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-25 23:00:18 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev -  prints a string, in reverse
+ * @s: string contains in 4-main.c file
+ * Return: no return
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len ; i >= 0 ; i--)
+	{
+		_putchar(*(s + i));
+	}
+}
