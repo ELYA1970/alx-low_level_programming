c16576eaec625c64aacde56006904b5f3af9959e
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-25 22:23:46 -0700

    correct the error in task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - take a number and reset to 98
- * @*n: pointe to a number entered by user
+ * @n: pointe to a number entered by user
  * Return: no return
  */
 void reset_to_98(int *n)
