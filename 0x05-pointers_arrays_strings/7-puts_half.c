1a4259598d3519944da9d1291e7ee7c5a5ffd624
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-26 00:05:21 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - that prints half of a string
+ * @str: string
+ * Return: no return
+ */
+void puts_half(char *str)
+{
+	int n = strlen(str);
+	int i, x;
+
+	if ((n % 2) != 0)
+	x = (strlen(str) - 1) / 2;
+	else
+	x = strlen(str) / 2;
+	for (i = x ; i < n ; i++)
+	putchar(*(str + i));
+	putchar('\n');
+}
