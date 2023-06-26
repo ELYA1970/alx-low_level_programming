b44dbf225674ab314656f9350a378e4cd4d958c5
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-26 04:53:32 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,9 +4,10 @@
  * _strcpy - that copies the string pointed to by src to dest
  * @src: string
  * @dest: string
+ * Return: return a string aftrer copie
  */
 char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
-	return dest;
+	return (dest);
 }
