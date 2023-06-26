08e4ef42a66cb5c359d764db4bd907493b52d6f9
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-26 00:09:06 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,12 @@ void puts_half(char *str)
 	int n = strlen(str);
 	int i, x;
 
-	if ((n % 2) != 0)
+	if ((n % 2) != 0){
 	x = (strlen(str) - 1) / 2;
+	for (i
 	else
 	x = strlen(str) / 2;
-	for (i = x ; i < n ; i++)
+	for (i = x ; i <= n - 1 ; i++)
 	putchar(*(str + i));
 	putchar('\n');
 }
