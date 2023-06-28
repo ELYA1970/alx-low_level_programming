26395ebe4b1dfed2502b7cb2c609b4984b23e2bd
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-06-28 03:05:49 -0700

    another answer

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,11 @@ void puts_half(char *str)
 	int n = strlen(str);
 	int i, x;
 
-	if (n + 1 % 2 != 0)
+	if (n  % 2 != 0)
 	x = (strlen(str) - 1) / 2;
 	else
 	x = strlen(str) / 2;
-	for (i = x + 1 ; i <= n - 1 ; i++)
+	for (i = x  ; i <= n - 1 ; i++)
 	putchar(*(str + i));
 	putchar('\n');
 }
