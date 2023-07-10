ff436cfc35b16d9d6e05141511377972efad7360
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-07-10 07:20:48 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *p = malloc(sizeof(char) * size);
 
-	if (size == 0)
+	if (size == 0 || p == 0)
 		return (NULL);
 	for (i = 0 ; i < size ; i++)
 		*(p + i) = c;
